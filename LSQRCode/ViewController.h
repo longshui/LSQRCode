94950ffde61deecf1e440853c7cf21926aace20e
git <longshuijian@gmail.com>
2016-06-21 19:12:00 +0800

    Initial Commit

---
+++ LSQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSQRCode
+//
+//  Created by 龙水健 on 16/6/21.
+//  Copyright © 2016年 longshui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
